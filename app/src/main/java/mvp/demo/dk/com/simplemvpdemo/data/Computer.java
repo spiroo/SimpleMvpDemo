4fe44df010d44fd6b7b07f7eaa7ddf45d3d89fcf
DK <spiroo@126.com>
2016-08-23 13:03:43 +0800

    update data

--- app/src/main/java/mvp/demo/dk/com/simplemvpdemo/data/Computer.java
+++ app/src/main/java/mvp/demo/dk/com/simplemvpdemo/data/Computer.java
@@ -5,4 +5,37 @@ package mvp.demo.dk.com.simplemvpdemo.data;
  */
 
 public class Computer {
+    private String name;
+    private double price;
+    private String model;
+
+    public Computer(String name, double price, String model) {
+        this.name = name;
+        this.price = price;
+        this.model = model;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
--- app/src/main/java/mvp/demo/dk/com/simplemvpdemo/data/ComputerFactory.java
+++ app/src/main/java/mvp/demo/dk/com/simplemvpdemo/data/ComputerFactory.java
@@ -1,8 +1,32 @@
 package mvp.demo.dk.com.simplemvpdemo.data;
 
+import java.util.ArrayList;
+
 /**
  * Created by DK-dong on 2016/8/23.
+ * 电脑生产工厂类
  */
 
 public class ComputerFactory {
+
+    private ArrayList<Computer> computers = new ArrayList<>();
+
+    public ArrayList<Computer> getComputers() {
+        return computers;
+    }
+
+    public void setComputers(ArrayList<Computer> computers) {
+        this.computers = computers;
+    }
+
+    public void addComputer(Computer computer){
+        computers.add(computer);
+    }
+
+    public void removeComputer(int index){
+        if (index >= 0 && index < computers.size()){
+            computers.remove(index);
+        }
+    }
+
 }
