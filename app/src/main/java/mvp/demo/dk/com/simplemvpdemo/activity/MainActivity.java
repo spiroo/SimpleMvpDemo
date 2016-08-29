81ef0d9d36ac80f94be98a19846e1fb8ef85c5c7
DK <spiroo@126.com>
2016-08-29 09:40:28 +0800

    modify directory

---
+++ app/src/main/java/mvp/demo/dk/com/simplemvpdemo/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package mvp.demo.dk.com.simplemvpdemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mvp.demo.dk.com.simplemvpdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
